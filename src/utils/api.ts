8c9afcc530b098931668448ba4ce9331c7671d17
iamnazeef <muhammednazeef6@gmail.com>
2023-06-25 12:45:38 +0530

    Updated api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,7 +3,7 @@ export const api = {
     if (latitude === null || longitude === null) return null;
 
     return `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${
-      import.meta.env.VITE_WEATHER_API_KEY
+      import.meta.env.VITE_OPENWEATHERMAP_API_KEY
     }&units=metric`;
   },
 };
