d4a2d8191dfd105441eabe47e506f40fa6a5c687
iamnazeef <muhammednazeef6@gmail.com>
2023-06-25 12:49:02 +0530

    styles update

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -1,6 +1,12 @@
 import { useState } from "react";
+import PlaceHolderImage from "../assets/images/placeholder-image.webp";
+import { Article } from "../ts/types";
 
-const News = ({ data }: any) => {
+interface Props {
+  article: Article;
+}
+
+const News = ({ article }: Props) => {
   const [isMouseOver, setIsMouseOver] = useState(false);
 
   return (
@@ -9,23 +15,29 @@ const News = ({ data }: any) => {
       onMouseOver={() => setIsMouseOver(true)}
       onMouseLeave={() => setIsMouseOver(false)}
     >
-      <section className="text w-[60%]">
-        <div
+      <section className="text w-[60%] relative">
+        <h2
           className={`title font-domine font-[700] text-base tablet:text-xl laptop:text-2xl mb-2 ${
             isMouseOver ? "underline" : "no-underline"
           }`}
         >
-          {data?.title}
-        </div>
-        <div className="description hidden tablet:block font-noto-sans-georgian text-sm laptop:text-base">
-          {data?.description.substring(0, 100)}...
+          {article?.title}
+        </h2>
+        <p className="description hidden tablet:block font-noto-sans-georgian text-sm laptop:text-base">
+          {article?.description.substring(0, 100)}...
+        </p>
+        <div className="date text-sm mt-2 text-gray-600 font-noto-sans-georgian">
+          <span className="whitespace-nowrap mr-1">Published on:</span>
+          <span className="whitespace-nowrap">
+            {article?.publishedAt?.substring(0, 10)}
+          </span>
         </div>
       </section>
       <section className="image w-[40%]">
         <img
-          src={data?.image}
+          src={article?.image ? article?.image : PlaceHolderImage}
           alt="image"
-          className="w-full max-w-[350px]"
+          className="w-full max-w-[350px] h-full object-contain"
           width={200}
           height={200}
         />
