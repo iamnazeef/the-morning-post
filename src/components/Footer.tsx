b5e7fdae343dfd929ac40b767710483e0dfddbaf
iamnazeef <muhammednazeef6@gmail.com>
2023-06-25 12:51:06 +0530

    Added 'select' option for selection of language

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,17 @@
 const Footer = () => {
   return (
-    <footer className="h-36 border-t border-black p-2">
+    <footer className="h-52 border-t border-black p-2">
+      <select className="border border-black mb-4 font-noto-sans-georgian">
+        <option value="en">English</option>
+        <option value="en">French</option>
+        <option value="en">Spanish</option>
+        <option value="en">Hindi</option>
+        <option value="en">Malayalam</option>
+      </select>
+      <div className="font-domine w-fit underline hover:no-underline cursor-pointer">
+        Back to top
+      </div>
       <div className="container mx-auto px-2">
-        <span className="font-domine underline hover:no-underline cursor-pointer">
-          Back to top
-        </span>
         <div className="text-center mt-8 font-noto-sans-georgian">
           The Morning Post &copy; 2023
         </div>
