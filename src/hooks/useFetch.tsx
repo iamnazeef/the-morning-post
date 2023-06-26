5d98024ec2ced2e67797f29dc6d7ec255bfc9dc2
iamnazeef <muhammednazeef6@gmail.com>
2023-06-26 14:37:34 +0530

    Updated fetch with axios

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,32 +1,46 @@
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import axios from "axios";
+import { api } from "../utils/api";
+import { LanguageContext } from "../context/LanguageContext";
+import { useSearchParams } from "react-router-dom";
+import { CountryContext } from "../context/CountryContext";
 
-const useFetch = (URL: string | null) => {
-  const controller = new AbortController();
+const useFetch = (dependencies: string[]) => {
   const [data, setData] = useState<any>({});
   const [error, setError] = useState<string>("");
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [searchParams] = useSearchParams();
+
+  const query = searchParams.get("q") || "None";
+  const category = searchParams.get("category") || "general";
+  const { language } = useContext(LanguageContext) || { language: "en" };
+  const { country } = useContext(CountryContext) || { country: "in" };
 
   useEffect(() => {
     let isMounted = true;
+    const controller = new AbortController();
+    if (isMounted) {
+      setData([]);
+      setError("");
+    }
 
     if (isMounted) {
-      setError("");
-      setIsLoading(true);
+      let URL = "";
+      if (query) {
+        URL = api.news(query, country, category, language);
+      }
       if (URL) {
+        setIsLoading(true);
         axios
-          .get(URL, {
-            signal: controller.signal,
-          })
+          .get(URL, { signal: controller.signal })
           .then((response) => {
             if (isMounted) {
-              setData(response.data);
+              setData(response.data.articles);
             }
           })
-          .catch((err) => {
+          .catch((error) => {
             if (isMounted) {
-              setError(err.message);
+              setError(error.message);
             }
           })
           .finally(() => {
@@ -41,7 +55,7 @@ const useFetch = (URL: string | null) => {
       controller.abort();
       isMounted = false;
     };
-  }, [URL]);
+  }, dependencies);
 
   return { data, error, isLoading };
 };
