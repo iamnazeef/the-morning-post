b8a22b525efdf1668b46b63eb0ca30fae536ed27
iamnazeef <muhammednazeef6@gmail.com>
2023-06-26 14:37:01 +0530

    Updated weather api

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,10 @@
 export const api = {
-  weather(longitude: number | null, latitude: number | null) {
-    if (latitude === null || longitude === null) return null;
-
+  news(query: string, country: string, category: string, lang: string) {
+    return `https://gnews.io/api/v4/top-headlines?country=${country}&category=${category}&lang=${lang}&q=${query}&apikey=${
+      import.meta.env.VITE_GNEWS_API_KEY
+    }`;
+  },
+  weather(longitude: number = 77.209, latitude: number = 28.6139) {
     return `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${
       import.meta.env.VITE_OPENWEATHERMAP_API_KEY
     }&units=metric`;
