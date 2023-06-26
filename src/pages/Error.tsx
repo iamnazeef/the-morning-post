75492535513626926d7b1ae9318044673c36695a
iamnazeef <muhammednazeef6@gmail.com>
2023-06-26 14:31:53 +0530

    Added error page

---
+++ src/pages/Error.tsx
@@ -0,0 +1,11 @@
+const Error = () => {
+  return (
+    <main className="home font-noto-sans-georgian min-h-[85vh] flex items-center justify-center text-lg laptop:text-xl">
+      <span className="font-bold">404</span>
+      <div className="h-[24px] w-[1px] bg-gray-400 mx-2"></div>
+      <span>This page could not be found</span>
+    </main>
+  );
+};
+
+export default Error;
