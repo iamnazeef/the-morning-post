89445e344bd31e3de1cb6ad7de5331192b3ee1cb
iamnazeef <muhammednazeef6@gmail.com>
2023-06-26 14:35:02 +0530

    Added language selection

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,43 @@
+import { useContext } from "react";
+import { LanguageContext } from "../context/LanguageContext";
+
 const Footer = () => {
+  const languageContextValue = useContext(LanguageContext);
+
+  if (languageContextValue === null) {
+    return null;
+  }
+
+  const { language, setLanguage } = languageContextValue;
+
+  const languages = [
+    { name: "english", code: "en" },
+    { name: "arabic", code: "ar" },
+    { name: "chinese", code: "zh" },
+    { name: "dutch", code: "nl" },
+    { name: "malayalam", code: "ml" },
+    { name: "french", code: "fr" },
+    { name: "spanish", code: "es" },
+  ];
+
+  const languageOptions = languages.map((language) => (
+    <option key={language.code} value={language.code}>
+      {language.name}
+    </option>
+  ));
+
+  const handleSelect = (event: any) => {
+    setLanguage(event.target.value);
+  };
+
   return (
     <footer className="h-52 border-t border-black p-2">
-      <select className="border border-black mb-4 font-noto-sans-georgian">
-        <option value="en">English</option>
-        <option value="en">French</option>
-        <option value="en">Spanish</option>
-        <option value="en">Hindi</option>
-        <option value="en">Malayalam</option>
+      <select
+        value={language}
+        onChange={handleSelect}
+        className="border border-black mb-4 font-noto-sans-georgian capitalize"
+      >
+        {languageOptions}
       </select>
       <div className="font-domine w-fit underline hover:no-underline cursor-pointer">
         Back to top
